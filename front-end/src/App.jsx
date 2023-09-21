f4d5be2215478ba0908ff69299a6facd3c8fa173
T.Phaneendra Nikhil <tallapragadaphaneendranikhil@gmail.com>
2023-09-21 12:16:44 +0530

    navbar

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from './components/nav/Navbar'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello World
+      <Navbar />
     </>
   )
 }
